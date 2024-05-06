eb0c72da43bfe8694a17406b7d6e3344f8c70b84
Andrija Obradovic <andrija.obradovic@student.um.si>
2024-05-06 22:49:59 +0200

    added mainKt

---
+++ web-scraper/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("it.skrape:skrapeit:1.2.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
