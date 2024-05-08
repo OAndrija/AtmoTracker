bad34ff9d98164b4f02e1b2eaf083c81639801c9
Andrija Obradovic <andrija.obradovic@student.um.si>
2024-05-08 17:10:51 +0200

    ATMOS-8 # moved code to backend folder

---
+++ backend/app.js
@@ -0,0 +1,84 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/userRoutes');
+var photosRouter = require('./routes/photoRoutes');
+
+var app = express();
+
+// MongoDB Atlas connection URI
+var mongoDB = "mongodb+srv://stanojabozinov:nojco123@p1.allgmev.mongodb.net/?retryWrites=true&w=majority&appName=P1";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// CORS configuration
+var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+app.use(cors({
+  credentials: true,
+  origin: function(origin, callback){
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1){
+      var msg = "The CORS policy does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// Middleware setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Session configuration using MongoDB Atlas
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: mongoDB })
+}));
+
+// Make session variables accessible in all views
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
+
+// Routes setup
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/photos', photosRouter);
+
+// Error handling
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  //res.render('error');
+  res.json(err);
+});
+
+module.exports = app;
