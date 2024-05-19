32ef451ae2854932370ea131eb9e22ebce5f5492
Andrija Obradovic <andrija.obradovic@student.um.si>
2024-05-19 17:52:48 +0200

    ATMOS-18 # created navbar and content tab for data, scraper and generator

--- admin/src/main/kotlin/Main.kt
+++ admin/src/main/kotlin/Main.kt
@@ -1,21 +1,22 @@
-import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -25,24 +26,56 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.rememberWindowState
 
-val barColor = Color(0xFF08DCC4)
-enum class MenuState { INVOICES, ABOUT_APP }
+val barColor = Color(0xFFA4BE5C)
+enum class MenuState { DATA, ABOUT_APP, SCRAPER, GENERATOR }
 
 @Composable
 fun Menu(menuState: MutableState<MenuState>, modifier: Modifier = Modifier) {
+    val borderColor = Color.Black
+    val borderWidth = 0.2.dp
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .background(barColor)
-    ) {
+            .drawBehind {
+                val strokeWidth = borderWidth.toPx()
+                val y = size.height - strokeWidth / 2
+                drawLine(
+                    color = borderColor,
+                    start = Offset(0f, y),
+                    end = Offset(size.width, y),
+                    strokeWidth = strokeWidth
+                )
+            }    ) {
         Box(
             modifier = Modifier
                 .height(50.dp)
                 .weight(1f)
-                .clickable { menuState.value = MenuState.INVOICES },
+                .clickable { menuState.value = MenuState.DATA },
             contentAlignment = Alignment.Center
         ) {
-            InvoicesMenuItem(menuState, modifier)
+            DataNavbarItem(menuState, modifier)
+        }
+
+        Box(
+            modifier = Modifier
+                .height(50.dp)
+                .weight(1f)
+                .clickable { menuState.value = MenuState.SCRAPER },
+            contentAlignment = Alignment.Center
+        ) {
+            ScraperNavbarItem(menuState, modifier)
+        }
+
+        Box(
+            modifier = Modifier
+                .height(50.dp)
+                .weight(1f)
+                .clickable { menuState.value = MenuState.GENERATOR },
+            contentAlignment = Alignment.Center
+        ) {
+            GeneratorNavbarItem(menuState, modifier)
         }
 
         Box(
@@ -52,28 +85,58 @@ fun Menu(menuState: MutableState<MenuState>, modifier: Modifier = Modifier) {
                 .clickable { menuState.value = MenuState.ABOUT_APP },
             contentAlignment = Alignment.Center
         ) {
-            AboutAppMenuItem(menuState, modifier)
+            AboutAppNavbarItem(menuState, modifier)
         }
     }
 }
 
 @Composable
-fun InvoicesMenuItem(menuState: MutableState<MenuState>, modifier: Modifier) {
+fun DataNavbarItem(menuState: MutableState<MenuState>, modifier: Modifier) {
     val invoiceIcon: Painter = painterResource("images/menu_icon.png")
 
     Row(verticalAlignment = Alignment.CenterVertically) {
         Image(
             painter = invoiceIcon,
-            contentDescription = "Invoices",
+            contentDescription = "Data",
             modifier = Modifier.size(24.dp)
         )
         Spacer(modifier = Modifier.size(4.dp))
-        Text(text = "Invoices")
+        Text(text = "Data")
     }
 }
 
 @Composable
-fun AboutAppMenuItem(menuState: MutableState<MenuState>, modifier: Modifier) {
+fun ScraperNavbarItem(menuState: MutableState<MenuState>, modifier: Modifier) {
+    val scraperIcon: Painter = painterResource("images/scraper_icon.png")
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = scraperIcon,
+            contentDescription = "Scraper",
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(text = "Scraper")
+    }
+}
+
+@Composable
+fun GeneratorNavbarItem(menuState: MutableState<MenuState>, modifier: Modifier) {
+    val generatorIcon: Painter = painterResource("images/generator_icon.png")
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = generatorIcon,
+            contentDescription = "Generator",
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(text = "Generator")
+    }
+}
+
+@Composable
+fun AboutAppNavbarItem(menuState: MutableState<MenuState>, modifier: Modifier) {
     val aboutAppIcon: Painter = painterResource("images/about_app_icon.png")
 
     Row(verticalAlignment = Alignment.CenterVertically) {
@@ -83,13 +146,27 @@ fun AboutAppMenuItem(menuState: MutableState<MenuState>, modifier: Modifier) {
             modifier = Modifier.size(24.dp)
         )
         Spacer(modifier = Modifier.size(4.dp))
-        Text(text = "About App")
+        Text(text = "About")
     }
 }
 @Composable
-fun InvoicesTab(modifier: Modifier = Modifier) {
+fun DataTab(modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("INVOICES CONTENT")
+        Text("DATA")
+    }
+}
+
+@Composable
+fun ScraperTab(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("SCRAPER")
+    }
+}
+
+@Composable
+fun GeneratorTab(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("GENERATOR")
     }
 }
 
@@ -105,56 +182,38 @@ fun AboutAppTab(modifier: Modifier = Modifier) {
         Text("About application", style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold))
         Spacer(modifier = Modifier.height(8.dp))
         Text("Subject: Principles of programming languages")
-        Text("Author: Matej Moravec")
+        Text("Author: Digitalni Trojcki")
     }
 }
 
 @Composable
 fun Content(menuState: MutableState<MenuState>, modifier: Modifier = Modifier) {
     when (menuState.value) {
-        MenuState.INVOICES -> InvoicesTab(modifier = modifier)
+        MenuState.DATA -> DataTab(modifier = modifier)
+        MenuState.SCRAPER -> ScraperTab(modifier = modifier)
+        MenuState.GENERATOR -> GeneratorTab(modifier = modifier)
         MenuState.ABOUT_APP -> AboutAppTab(modifier = modifier)
-    }
-}
-
-@Composable
-fun Footer(menuState: MutableState<MenuState>, modifier: Modifier = Modifier) {
-    val text = when (menuState.value) {
-        MenuState.INVOICES -> "You're viewing the \"Invoices\" tab."
-        MenuState.ABOUT_APP -> "You're viewing the \"About App\" tab."
-    }
-
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(25.dp)
-            .background(barColor),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Text(text = text)
     }
 }
 
 @Composable
 @Preview
 fun App() {
-    val menuState = remember { mutableStateOf(MenuState.ABOUT_APP) }
+    val menuState = remember { mutableStateOf(MenuState.DATA) }
 
     Column {
         Menu(menuState)
         Content(menuState = menuState, modifier = Modifier.weight(1f))
-        Footer(menuState)
     }
 }
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Vodjenje racunov",
+        title = "Admin App",
         state = rememberWindowState(
             position = WindowPosition.Aligned(Alignment.Center),
-            size = DpSize(900.dp, 800.dp)
+            size = DpSize(800.dp, 700.dp)
         ),
         undecorated = false,
         resizable = true
