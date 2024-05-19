9f8509428e98cdc89db8c57c971d6b6edbbfa14b
Stanoja Bozinov <stanoja.bozinov@student.um.si>
2024-05-19 15:44:36 +0200

    ATMOS-16 # Commit buld Gradle

--- web-scraper/build.gradle.kts
+++ web-scraper/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.6.10"
+
 }
 
 group = "org.example"
@@ -12,6 +14,16 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("it.skrape:skrapeit:1.2.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation ("com.github.kittinunf.fuel:fuel:2.3.1") // for HTTP operations
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.3.1")  // for coroutine support in Fuel
+
+
+
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
 }
 
 tasks.test {
