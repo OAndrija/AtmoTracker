49ef417d1ceffcbcc38068508446242d1635b1b3
Martin Paunoski <162042202+martin-paunoski@users.noreply.github.com>
2024-05-22 21:34:10 +0200

    Revert "Pull request for Atmos 83 generator implementation"

--- admin/src/main/kotlin/AirQuality.kt
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AirQuality(
-    val name: String?,
-    @Serializable(with = LocalDateTimeSerializer::class) val timestamp: LocalDateTime = LocalDateTime.now(),
-    val data: Map<String, String?>
-)
