2f8d397bc3d222ea75635ddcc96b7c1a4ec2df18
Andrija Obradovic <andrija.obradovic@student.um.si>
2024-05-30 10:31:08 +0200

    test 7

--- backend/app.js
+++ backend/app.js
@@ -94,7 +94,7 @@ const broadcastData = async (ws = null) => {
       throw new Error('Network response was not ok');
     }
     const data = await response.json();
-    console.log('Data fetched succesfuly'); // Log the fetched data
+    console.log('Data fetched succesfuly'); // Log the fetched datadsada
     const dataToSend = JSON.stringify(data);
 
     if (ws) {
