a2e6b994ea3c2879228a228d6ca7cc2d9cff0d34
martin. <martinpaunoski7415@gmail.com>
2024-06-02 20:20:57 +0200

    ATMOS-18 # fixed the Generator and added a Send button and fixed the output of the Data

--- admin/src/main/kotlin/MongoDBClient.kt
+++
@@ -1,8 +0,0 @@
-import com.mongodb.client.MongoClients
-import com.mongodb.client.MongoDatabase
-
-object MongoDBClient {
-    private const val CONNECTION_STRING = "mongodb+srv://stanojabozinov:nojco123@p1.allgmev.mongodb.net/?retryWrites=true&w=majority&appName=P1"
-    private val client = MongoClients.create(CONNECTION_STRING)
-    val database: MongoDatabase = client.getDatabase("test")
-}
