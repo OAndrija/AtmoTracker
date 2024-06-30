c8f00563b5ba27454865a71e7064869fdd365582
martin. <martinpaunoski7415@gmail.com>
2024-06-30 22:27:09 +0200

    ATMOS-11 # implemented icons for the MapComponent

--- frontend/src/scenes/map/MapComponent.js
+++ frontend/src/scenes/map/MapComponent.js
@@ -29,113 +29,197 @@ import { tokens } from "../../theme";
 const DEFAULT_CENTER = [46.13723571213672, 14.732964599882179];
 const DEFAULT_ZOOM = 9;
 
+
 const defaultIcon = new L.Icon({
-  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-  iconSize: [28, 44],
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 
 const greenPm10Icon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=material&color=green&size=small&icon=factory&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=material&color=green&size=small&icon=factory&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 
 const orangePm10Icon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=material&color=orange&size=small&icon=factory&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=material&color=orange&size=small&icon=factory&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 const redPm10Icon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=material&color=red&size=small&icon=factory&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=material&color=red&size=small&icon=factory&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 const greenpm25Icon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=green&size=small&icon=smog&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=green&size=small&icon=smog&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 
 const orangepm25Icon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=orange&size=small&icon=smog&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=orange&size=small&icon=smog&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 
 const redpm25Icon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=red&size=small&icon=smog&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=red&size=small&icon=smog&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 
 const greenOzon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=green&size=small&icon=shield-alt&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=green&size=small&icon=shield-alt&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 const orangeOzon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=orange&size=small&icon=shield-alt&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=orange&size=small&icon=shield-alt&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 const redOzon = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=red&size=small&icon=shield-alt&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=red&size=small&icon=shield-alt&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 const greenNo2 = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=green&size=small&icon=car&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=green&size=small&icon=car&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 const orangeNo2 = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=orange&size=small&icon=car&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=orange&size=small&icon=car&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
 });
 const redNo2 = new L.Icon({
-  iconUrl:
-    "https://api.geoapify.com/v1/icon/?type=awesome&color=red&size=small&icon=car&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3",
-  iconSize: [28, 44],
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=red&size=small&icon=car&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  shadowSize: [41, 41]
+});
+
+const snowFlake = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=blue&size=small&icon=snowflake&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const temperatureLow = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=%23add8e6&size=small&icon=temperature-low&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const cloudSun = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=material&color=orange&size=small&icon=cloud-sun&iconType=awesome&iconSize=small&textSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const sonce = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=yellow&size=small&icon=sun&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const temperatureHigh = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=red&size=small&icon=temperature-high&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const padovinaSunIcon = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=%23add8e6&size=small&icon=sun&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const lightRain = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=blue&size=small&icon=cloud-sun&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const moderateRain = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=%2300008b&size=small&icon=cloud-showers-heavy&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const haevyRain = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=purple&size=small&icon=cloud-showers-heavy&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const calmWind = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=lightgreen&size=small&icon=wind&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const lightWind = new L.Icon({
+  iconUrl:  'https://api.geoapify.com/v1/icon/?type=awesome&color=green&size=small&icon=wind&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const moderateWind = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=%23f4ce00&size=small&icon=wind&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const heavyWind = new L.Icon({
+  iconUrl: 'https://api.geoapify.com/v1/icon/?type=awesome&color=%23ec9d00&size=small&icon=wind&iconSize=small&scaleFactor=2&apiKey=2b257b2140204b5abc2a90177e0a63d3',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
 });
 
 const SetViewOnChange = ({ center, zoom }) => {
@@ -204,7 +288,7 @@ const MapComponent = ({
       case "pm25":
         return item.pm25 !== undefined && item.pm25 !== "";
       case "ozon":
-        return item.ozon !== undefined && item.ozon !== "";
+        return item.ozon !== undefined && item.ozon !== "" && item.ozon !== "-";
       case "no2":
         return item.no2 !== undefined && item.no2 !== "";
       default:
@@ -241,7 +325,43 @@ const MapComponent = ({
   };
 
   const getIcon = (item) => {
-    if (filter === "pm10") {
+
+    if  (filter === 'temperature') {
+      if (item.temperature < 0) {
+        return snowFlake;
+      } else if (item.temperature >= 0 && item.temperature < 10) {
+        return temperatureLow;
+      } else if (item.temperature >= 10 && item.temperature < 20) {
+        return cloudSun;
+      } else if (item.temperature >= 20 && item.temperature < 30) {
+        return sonce;
+      } else if (item.temperature >= 30) {
+        return temperatureHigh;
+      }
+    }
+    else if (filter === 'rain') {
+      if (item.precipitation == 0) {
+        return padovinaSunIcon;
+      } else if (item.precipitation > 0 && item.precipitation <= 2.5) {
+        return lightRain;
+      } else if (item.precipitation > 2.5 && item.precipitation <= 7.5) {
+        return moderateRain;
+      } else if (item.precipitation > 7.5 && item.precipitation <= 50) {
+        return haevyRain;
+      }
+    }
+    else if (filter === 'wind') {
+      if (item.windSpeed <= 2) {
+        return calmWind;
+      } else if (item.windSpeed <= 5.5) {
+        return lightWind;
+      } else if (item.windSpeed <= 10) {
+        return moderateWind;
+      } else if (item.windSpeed > 10) {
+        return heavyWind;
+      }
+    }
+    else if  (filter === 'pm10') {
       if (item.pm10 <= 54) {
         return greenPm10Icon;
       } else if (item.pm10 <= 254) {
@@ -249,7 +369,7 @@ const MapComponent = ({
       } else if (item.pm10 <= 354) {
         return redPm10Icon;
       }
-    } else if (filter === "pm25") {
+    } else if (filter === 'pm25') {
       if (item.pm25 <= 12) {
         return greenpm25Icon;
       } else if (item.pm25 <= 55.4) {
@@ -257,7 +377,7 @@ const MapComponent = ({
       } else if (item.pm25 <= 150.4) {
         return redpm25Icon;
       }
-    } else if (filter === "ozon") {
+    } else if (filter === 'ozon') {
       if (item.ozon <= 54) {
         return greenOzon;
       } else if (item.ozon <= 85) {
@@ -267,14 +387,15 @@ const MapComponent = ({
       } else {
         return redOzon;
       }
-    } else if (filter === "no2") {
+    } else if (filter === 'no2') {
       if (item.no2 <= 53) {
         return greenNo2;
-      } else if (item.no2 <= 360) {
+      }  else if (item.no2 <= 360) {
         return orangeNo2;
       } else if (item.no2 <= 649) {
         return redNo2;
-      } else {
+      }
+      else {
         return redNo2;
       }
     }
