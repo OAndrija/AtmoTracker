5481eb5fbcfe03e0a68f17571e9fc59b57ff4954
stanoja1234 <stanoja.bozinov@student.um.si>
2025-01-21 08:52:04 +0100

    added EventFragment

--- app/src/main/java/com/example/atmotracker/fragments/EventFragment.kt
+++ app/src/main/java/com/example/atmotracker/fragments/EventFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import com.example.atmotracker.R
 
 class EventFragment : Fragment() {
-    override fun onCreateView(
+     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
