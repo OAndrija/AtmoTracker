a0432f288c415cdc41f9b0d276487d3370fc4190
stanoja1234 <stanoja.bozinov@student.um.si>
2025-01-21 08:47:45 +0100

    added EventFragment

--- app/src/main/java/com/example/atmotracker/fragments/EventFragment.kt
+++ app/src/main/java/com/example/atmotracker/fragments/EventFragment.kt
@@ -5,9 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.recyclerview.widget.RecyclerView
 import com.example.atmotracker.R
 
 class EventFragment : Fragment() {
+
+    private lateinit var editTextMessage: EditText
+    private lateinit var editTextTime: EditText
+    private lateinit var editTextLocation: EditText
+    private lateinit var spinnerTopic: Spinner
+    private lateinit var buttonSubmit: Button
+    private lateinit var recyclerViewEvents: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
